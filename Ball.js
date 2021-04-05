30d65de0e3d93e63adf1e39e60816f01f7f303e0
Darshit Singodia <73582939+DarshitSingodia@users.noreply.github.com>
2021-04-05 13:00:39 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,28 @@
+class Ball{
+    constructor(x, y, width, height, angle){
+
+        var options={
+            denisty : 1,
+            frictionAir : 0.005
+        }
+
+        this.width = width;
+        this.height = height;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        World.add(world, this.body);
+    }
+
+    display(){
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        fill("yellow") 
+        ellipse(0, 0, this.width, this.height);
+        pop();
+
+    }
+}
